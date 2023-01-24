9defa6acf631023469faa8698a3f65d820c04899
Nate Sticco <nsticco@gmail.com>
2023-01-24 13:59:14 -0800

    fix start-process syntax

--- Windows/InstallWazuh/InstallWazuh.ps1
+++ Windows/InstallWazuh/InstallWazuh.ps1
@@ -3,7 +3,7 @@ $WazuhManagerDNS = Read-Host "Enter the DNS address of the Wazuh Manager, e.g. w
 Write-Output "Installing Wazuh Agent..."
 Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.10.msi
 
-Start-Process /wait msiexec.exe /qn /norestart /i ${env:tmp}\wazuh-agent-4.3.10.msi WAZUH_MANAGER=$WazuhManagerDNS WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS WAZUH_AGENT_GROUP='default'
+Start-Process -FilePath msiexec.exe -ArgumentList '/qn', '/norestart', '/i', "$env:tmp\wazuh-agent-4.3.10.msi", "WAZUH_MANAGER=$WazuhManagerDNS", "WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS", "WAZUH_AGENT_GROUP='default'" -Wait
 
 Write-Output "Starting Wazuh Agent..."
 NET START WazuhSvc
