2395d1d031de27b800b7f17b0bc05e1f88342236
Nate Sticco <nsticco@gmail.com>
2023-01-24 15:16:24 -0800

    use wazuh command more closely

--- Windows/InstallWazuh/InstallWazuh.ps1
+++ Windows/InstallWazuh/InstallWazuh.ps1
@@ -9,7 +9,7 @@ if (!$WazuhManagerDNS) {
 Write-Output "Installing Wazuh Agent..."
 Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.10.msi
 
-Start-Process -FilePath msiexec.exe -ArgumentList '/qn', '/norestart', '/i', "$env:tmp\wazuh-agent-4.3.10.msi", "WAZUH_MANAGER=$WazuhManagerDNS", "WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS", "WAZUH_AGENT_GROUP='default'" -Wait
+Start-Process -FilePath msiexec.exe -ArgumentList '/q', '/norestart', '/i', "$env:tmp\wazuh-agent-4.3.10.msi", "WAZUH_MANAGER=$WazuhManagerDNS", "WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS", "WAZUH_AGENT_GROUP='default'" -Wait
 
 Write-Output "Starting Wazuh Agent..."
 NET START WazuhSvc
