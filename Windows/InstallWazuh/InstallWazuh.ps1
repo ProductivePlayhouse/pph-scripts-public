e401b595c1e172be1ef0b88c378413c8d27e697b
Nate Sticco <nsticco@gmail.com>
2023-01-24 13:55:49 -0800

    really wait for real

--- Windows/InstallWazuh/InstallWazuh.ps1
+++ Windows/InstallWazuh/InstallWazuh.ps1
@@ -1,9 +1,9 @@
 $WazuhManagerDNS = Read-Host "Enter the DNS address of the Wazuh Manager, e.g. wazuh.com"
 
 Write-Output "Installing Wazuh Agent..."
-Invoke-RestMethod -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.10.msi
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.10.msi
 
-msiexec.exe /i ${env:tmp}\wazuh-agent-4.3.10.msi /quiet /wait WAZUH_MANAGER=$WazuhManagerDNS WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS WAZUH_AGENT_GROUP='default'
+Start-Process /wait msiexec.exe /qn /norestart /i ${env:tmp}\wazuh-agent-4.3.10.msi WAZUH_MANAGER=$WazuhManagerDNS WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS WAZUH_AGENT_GROUP='default'
 
 Write-Output "Starting Wazuh Agent..."
 NET START WazuhSvc
