5df959a40541ce7e0f4085cc1e5393c29854cd46
Nate Sticco <nsticco@gmail.com>
2023-01-24 14:31:47 -0800

    fix param system

--- Windows/BootstrapBase/BootstrapBase.ps1
+++ Windows/BootstrapBase/BootstrapBase.ps1
@@ -1,6 +1,6 @@
 # Run this script from an elevated PowerShell prompt
 param(
-    [string]$WazuhManagerDNS
+	[string]$WazuhManagerDNS
 )
 
 # If $WazuhManagerDNS is not set, prompt the user for it
@@ -8,6 +8,7 @@ if (!$WazuhManagerDNS) {
 	$WazuhManagerDNS = Read-Host "Enter the DNS address of the Wazuh Manager, e.g. wazuh.com"
 }
 
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/Windows/InstallWazuh/InstallWazuh.ps1')) -WazuhManagerDNS $WazuhManagerDNS
+$WazuhScriptPath = ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/Windows/InstallWazuh/InstallWazuh.ps1'))
+Invoke-Command -ScriptBlock ([scriptblock]::Create($WazuhScriptPath)) -ArgumentList @{'WazuhManagerDNS'=$WazuhManagerDNS}
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/Windows/InstallChocolatey/InstallChocolatey.ps1'))
