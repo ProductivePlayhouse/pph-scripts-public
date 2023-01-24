9bc0dbd34acab326a298586b72c143492607a50d
Nate Sticco <nsticco@gmail.com>
2023-01-24 13:37:03 -0800

    add wait to wazuh

--- Windows/BootstrapBase/BootstrapBase.ps1
+++ Windows/BootstrapBase/BootstrapBase.ps1
@@ -1,4 +1,6 @@
 # Run this script from an elevated PowerShell prompt
+# Call this script remotely with:
+# Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/Windows/BootstrapBase/BootstrapBase.ps1'))
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/Windows/InstallWazuh/InstallWazuh.ps1'))
 
