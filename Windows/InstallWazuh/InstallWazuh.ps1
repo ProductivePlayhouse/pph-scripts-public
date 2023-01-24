53b9ab4fc8e8275931ef08bc38c0360ad9a2efe9
Nate Sticco <nsticco@gmail.com>
2023-01-24 15:28:25 -0800

    brute force DNS

--- Windows/InstallWazuh/InstallWazuh.ps1
+++ Windows/InstallWazuh/InstallWazuh.ps1
@@ -11,5 +11,11 @@ Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10
 
 Start-Process -FilePath msiexec.exe -ArgumentList '/q', '/norestart', '/i', "$env:tmp\wazuh-agent-4.3.10.msi", "WAZUH_MANAGER=$WazuhManagerDNS", "WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS", "WAZUH_AGENT_GROUP='default'" -Wait
 
+Write-Output "Making sure DNS is correct..."
+$filePath = "C:\Program Files (x86)\ossec-agent\ossec.conf"
+$xmlContent = Get-Content $filePath
+$xmlContent = $xmlContent -replace "<address>0.0.0.0</address>", "<address>$WazuhManagerDNS</address>"
+Set-Content -Value $xmlContent -Path $filePath
+
 Write-Output "Starting Wazuh Agent..."
 NET START WazuhSvc
