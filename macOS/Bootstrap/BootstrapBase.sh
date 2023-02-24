8f511884ff83ce3a21adbd3f552aa4c3ce952df9
Nate Sticco <nate.sticco@productiveplayhouse.com>
2023-02-24 13:22:48 -0800

    add git and wazuh param

--- macOS/Bootstrap/BootstrapBase.sh
+++ macOS/Bootstrap/BootstrapBase.sh
@@ -1,5 +1,16 @@
+if [ -z "$1" ]; then
+	# If no argument was provided, read DNS address from the user
+	echo "Enter the DNS address of the Wazuh Manager, e.g. wazuh.com: "
+	read WazuhManagerDNS
+else
+	# If an argument was provided, use it for DNS
+	WazuhManagerDNS=$1
+fi
+
 echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Wazuh
-curl -o wazuh.sh https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/macOS/Wazuh/InstallWazuh.sh && chmod +x wazuh.sh && bash ./wazuh.sh
+echo "Installing git..."
+brew install git
+
+curl -o wazuh.sh https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/macOS/Wazuh/InstallWazuh.sh && chmod +x wazuh.sh && bash ./wazuh.sh $WazuhManagerDNS
