bbeaac147a788a3fd4f5d9e4cfe4fa625a21ff41
natesticco <122649475+natesticco@users.noreply.github.com>
2023-06-26 09:33:05 -0700

    Create BootstrapDev.sh

---
+++ macOS/Bootstrap/BootstrapDev.sh
@@ -0,0 +1,8 @@
+# Install Homebrew
+curl -o homebrew.sh https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/macOS/Homebrew/InstallHomebrew.sh && chmod +x homebrew.sh && bash ./homebrew.sh
+
+# Install XCode
+curl -o xcode.sh https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/macOS/XCode/InstallXCode.sh && chmod +x xcode.sh && bash ./xcode.sh
+
+# Install Git
+curl -o git.sh https://raw.githubusercontent.com/ProductivePlayhouse/pph-scripts-public/main/macOS/Git/InstallGit.sh && chmod +x git.sh && bash ./git.sh
