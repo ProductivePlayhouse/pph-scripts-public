594a454d5c9fb4232562e83d9ec392503e55483d
natesticco <122649475+natesticco@users.noreply.github.com>
2024-08-19 15:44:54 -0700

    Update to 4.8.1 and make DRY

--- Windows/Wazuh/InstallWazuh.ps1
+++ Windows/Wazuh/InstallWazuh.ps1
@@ -2,14 +2,17 @@ param(
 	[string]$WazuhManagerDNS
 )
 
+# Define the Wazuh version as a variable
+$WazuhVersion = "4.8.1"
+
 if (!$WazuhManagerDNS) {
 	$WazuhManagerDNS = Read-Host "Enter the DNS address of the Wazuh Manager, e.g. wazuh.com"
 }
 
 Write-Output "Installing Wazuh Agent..."
-Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi -OutFile ${env:tmp}\wazuh-agent-4.3.10.msi
+Invoke-WebRequest -Uri "https://packages.wazuh.com/4.x/windows/wazuh-agent-$WazuhVersion-1.msi" -OutFile "${env:tmp}\wazuh-agent-$WazuhVersion.msi"
 
-Start-Process -Wait -FilePath msiexec.exe -ArgumentList '/q', '/norestart', '/i', "$env:tmp\wazuh-agent-4.3.10.msi", "WAZUH_MANAGER=$WazuhManagerDNS", "WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS"
+Start-Process -Wait -FilePath msiexec.exe -ArgumentList '/q', '/norestart', '/i', "$env:tmp\wazuh-agent-$WazuhVersion.msi", "WAZUH_MANAGER=$WazuhManagerDNS", "WAZUH_REGISTRATION_SERVER=$WazuhManagerDNS"
 
 Write-Output "Making sure DNS is correct..."
 $filePath = "C:\Program Files (x86)\ossec-agent\ossec.conf"
